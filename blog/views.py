d0849f84fe1bbbf81bec4a33f0ab5843d316f62e
gaurabn1 <gaurabneupane8969@gmail.com>
2025-01-25 15:49:45 +0545

    Added view and template for the detailed blog post as well as css for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Blog
 
@@ -8,7 +8,9 @@ def post_list(request):
     paginator = Paginator(posts, 3)
 
     page_number = request.GET.get('page', 1)
-    print(page_number)
     page_obj = paginator.get_page(page_number)
-    print(page_obj)
     return render(request, 'blog/post_list.html', {'page_obj': page_obj})
+
+def post_detail(request, id):
+    post = get_object_or_404(Blog, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
