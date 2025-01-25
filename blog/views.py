ee65247f20e0963a0ec89478d526fa6f255b2adc
gaurabn1 <gaurabneupane8969@gmail.com>
2025-01-25 14:48:40 +0545

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def post_list(request):
+    posts = Blog.objects.filter(published_date__isnull=False)
+    paginator = Paginator(posts, 10)
+
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
