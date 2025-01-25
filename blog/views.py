986b46f06abd947ac705c1db2fcfcc2850c0fd99
gaurabn1 <gaurabneupane8969@gmail.com>
2025-01-25 16:26:17 +0545

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Blog
+from .forms import BlogForm
 
 def post_list(request):
     posts = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -14,3 +15,31 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Blog, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = BlogForm()
+    return render(request, 'blog/post_new.html', {'form': form})
+
+def post_edit(request, id):
+    post = get_object_or_404(Blog, id=id)
+    if request.method =='POST':
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.id)
+
+    else:
+        form = BlogForm(instance=post)
+    return render(request, 'blog/post_new.html', {'form':form})
